cdb27883d270e6e752528fee281aec63011e5a47
willemserruys <willem.serruys@vinci-energies.net>
2018-11-18 21:55:55 +0100

    added UserResponse

--- WebApiSample.Api.21/Controllers/UserController.cs
+++ WebApiSample.Api.21/Controllers/UserController.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using HourRegistration.DataAccess.Models;
 using HourRegistration.DataAccess.Repositories;
+using System;
+using Microsoft.AspNetCore.Http;
 
 namespace HourRegistration.API
 {
@@ -14,11 +16,11 @@ namespace HourRegistration.API
         private readonly UserRepository _repository;
         private readonly ILogger _logger;
 
-            public UserController(UserRepository repository, ILogger<UserController> logger)
-            {
-                _repository = repository;
+        public UserController(UserRepository repository, ILogger<UserController> logger)
+        {
+            _repository = repository;
             _logger = logger;
-            }
+        }
 
         [HttpGet]
         public async Task<ActionResult<List<User>>> GetAllAsync()
@@ -29,14 +31,27 @@ namespace HourRegistration.API
             return users;
         }
 
-        [HttpPost]
-        
-        public async Task<ActionResult<User>> CreateAsync(string name, string password)
+        [HttpPost("{name}/{password}", Name = "Create")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(UserResponse))]
+        public async Task<ActionResult<UserResponse>> CreateAsync(string name, string password)
         {
-            _logger.LogDebug("Create User");
-            var user = await _repository.CreateAsync(name, password);
-
-            return user;
+            var usRes = new UserResponse();
+            _logger.LogDebug("Create User Request Received, name = {0}, password = {1}", name, password);
+            try
+            {
+                var user = await _repository.CreateAsync(name, password);
+                usRes.User = user;
+                usRes.IsCreated = true;
+                return usRes;
+            }
+            catch (Exception e)
+            {
+                var user = new User { UserName = name, PassWord = password };
+                usRes.User = user;
+                usRes.IsCreated = false;
+                usRes.FailedMessage = e.Message;
+                return usRes;
+            }
         }
     }
 
--- WebApiSample.DataAccess/Repositories/UserRepository.cs
+++ WebApiSample.DataAccess/Repositories/UserRepository.cs
@@ -3,21 +3,23 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using HourRegistration.DataAccess.Models;
-
+using System;
+using Microsoft.Extensions.Logging;
 
 namespace HourRegistration.DataAccess.Repositories
 {
+
     public class UserRepository
     {
+        private readonly ILogger _logger;
         private readonly UserContext _context;
 
-        public UserRepository(UserContext context)
+        public UserRepository(UserContext context, ILogger<UserContext> logger)
         {
             _context = context;
-
-
-
+            _logger = logger;
         }
+
         public async Task<List<User>> GetUsersAsync()
         {
             return await _context.Users.ToListAsync();
@@ -25,9 +27,20 @@ namespace HourRegistration.DataAccess.Repositories
 
         public async Task<User> CreateAsync(string name, string password)
         {
-            var user = await _context.AddAsync(new User { UserName = name, PassWord = password });
-            await _context.SaveChangesAsync();
-            return user.Entity;
+            try
+            {
+
+                var user = await _context.AddAsync(new User { UserName = name, PassWord = password });
+                await _context.SaveChangesAsync();
+                _logger.LogInformation("User Created with Name {0}", name);
+                return user.Entity;
+            }
+            catch (Exception e)
+            {
+                var errorMessage = string.Format("User not created: {0}", e.InnerException.Message);
+                _logger.LogError(errorMessage);
+                throw (new Exception(errorMessage));
+            };
         }
 
         public async Task<User> GetById(int id)
